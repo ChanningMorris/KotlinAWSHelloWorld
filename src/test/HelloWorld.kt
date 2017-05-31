b1ceb06faa029a7f1acfea83eb5ed55f0479bb3e
Channing Morris <richard.morris@bbva.com>
2017-05-31 08:14:59 -0500

    Initial Commit.

---
+++ src/test/HelloWorld.kt
@@ -0,0 +1,12 @@
+package test
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.LambdaLogger
+
+public class App {
+    public fun handler(count: Int, context: Context): String {
+        val lambdaLogger = context.getLogger()
+        lambdaLogger.log("count = " + count)
+        return "${count * 25}" + " Channing"
+    }
+}
